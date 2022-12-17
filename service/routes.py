38e536a01f46b301d60eaeca5cb78c65277835e3
DorisMmmm <116881280+DorisMmmm@users.noreply.github.com>
2022-12-17 14:40:09 +0100

    Removing obsolete blank lines and whitespaces
    as proposed by lint with flake8

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -69,7 +70,6 @@ def list_accounts():
     app.logger.info("Request to list all accounts")
 
     list_accounts = Account.all()
-    
     account_list = [account.serialize() for account in list_accounts]
 
     app.logger.info("Returning [%s] accounts", len(account_list))
@@ -78,6 +78,7 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -96,6 +97,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -110,11 +112,10 @@ def update_accounts(account_id):
     account.update()
     return account.serialize(), status.HTTP_200_OK
 
-
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
     """
@@ -130,6 +131,7 @@ def delete_account(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
+
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
